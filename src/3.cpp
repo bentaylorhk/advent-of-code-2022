4ae3c3eef49f5bb374ea9cc8589bb15595ca6ba0
Benjamin Taylor <benjamin.taylor@arm.com>
2022-12-09 15:17:14 +0000

    Day 3 Part 2

--- src/3.cpp
+++ src/3.cpp
@@ -5,12 +5,15 @@
 
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <set>
 
-int get_priority(char item) {
+int get_priority(char item)
+{
     if (item > 90) {
         return item - 96;
-    } else {
+    }
+    else {
         return item - 38;
     }
 }
@@ -22,20 +25,34 @@ int main(int argc, char * argv[])
 
     int sum = 0;
 
-    while (std::getline(file, line)) {
-        std::set<char> items;
-        int compartment_size = (int)line.size() / 2;
+    std::map<char, int> items_count;
 
-        for (int i = 0; i < compartment_size; i++) {
-            items.insert(line.at(i));
+    for (int elf_count = 0; std::getline(file, line); elf_count++) {
+        if (elf_count % 3 == 0 && elf_count != 0) {
+            for (auto & it : items_count) {
+                if (it.second == 3) {
+                    sum += get_priority(it.first);
+                    break;
+                }
+            }
+
+            items_count.clear();
         }
 
-        for (int i = compartment_size; i < line.size(); i++) {
-            char item = line.at(i);
-            if (items.contains(item)) {
-                sum += get_priority(item);
-                break;
+        std::set<char> elf_items;
+
+        for (char & c : line) {
+            if (!elf_items.contains(c)) {
+                items_count[c]++;
+                elf_items.insert(c);
             }
+        }
+    }
+    
+    for (auto & it : items_count) {
+        if (it.second == 3) {
+            sum += get_priority(it.first);
+            break;
         }
     }
 
