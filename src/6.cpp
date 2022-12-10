89ada9b65b28b3ad69d6f06b8d2ddd5a7cce8b24
Benjamin Taylor <benjamin.taylor@arm.com>
2022-12-10 15:34:56 +0000

    Day 6 Part 1

---
+++ src/6.cpp
@@ -0,0 +1,43 @@
+/**
+ * Benjamin Michael Taylor
+ * 2022
+ */
+
+#include <fstream>
+#include <iostream>
+#include <queue>
+
+int main(int argc, char * argv[])
+{
+    std::ifstream file(INPUT_FILENAME);
+    std::string line;
+
+    std::getline(file, line);
+
+    std::deque<char> deque;
+
+    for (int i = 0; i < line.size(); i++) {
+        char c = line.at(i);
+
+        std::deque<char> next_dequeue = deque;
+
+        while (!next_dequeue.empty()) {
+            char next_c = next_dequeue.front();
+            next_dequeue.pop_front();
+
+            if (next_c == c) {
+                deque = next_dequeue;
+                break;
+            }
+        }
+
+        deque.push_back(c);
+
+        if (deque.size() == 4) {
+            printf("%d\n", i + 1);
+            break;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ template.cpp
@@ -0,0 +1,18 @@
+/**
+ * Benjamin Michael Taylor
+ * 2022
+ */
+
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+    std::ifstream file(INPUT_FILENAME);
+    std::string line;
+
+    while (std::getline(file, line)) {
+    }
+
+    return EXIT_SUCCESS;
+}
