337dc4b4baff510f771a98ffb82f827195f373f0
Benjamin Taylor <bentaylorhk@gmail.com>
2022-12-18 22:27:14 +0000

    Day 9 Part 1

--- src/9.cpp
+++ src/9.cpp
@@ -11,14 +11,16 @@
 #include <regex>
 #include <vector>
 
-enum Direction { U, R, D, L };
+const int GRID_SIZE = 400;
+
+enum Direction { U = 0, R = 1, D = 2, L = 3 };
 
 struct Position {
-    int x = 0;
-    int y = 0;
+    int x = 200;
+    int y = 200;
 };
 
-std::vector<struct Position> offsets({{1, 0}, {0, 1}, {-1, 0}, {0, -1}});
+std::vector<struct Position> offsets({{0, 1}, {1, 0}, {0, -1}, {-1, 0}});
 
 std::map<char, Direction> charMap{{'U', U}, {'R', R}, {'D', D}, {'L', L}};
 
@@ -26,11 +28,21 @@ bool isAdjacent(struct Position p1, struct Position p2) {
     return std::abs(p1.x - p2.x) <= 1 && std::abs(p1.y - p2.y) <= 1;
 }
 
+void printVisited(std::vector<std::vector<bool>> visited) {
+    for (int y = visited.size() - 1; y >= 0; y--) {
+        for (int x = 0; x < visited[0].size(); x++) {
+            printf("%c", visited[y][x] ? '#' : '.');
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main(int argc, char* argv[]) {
     std::ifstream file(INPUT_FILENAME);
     std::string line;
 
-    std::vector<std::vector<bool> > visited(100, std::vector<bool>(100, false));
+    std::vector<std::vector<bool> > visited(GRID_SIZE, std::vector<bool>(GRID_SIZE, false));
 
     struct Position head, tail;
 
@@ -58,6 +70,7 @@ int main(int argc, char* argv[]) {
                 visited[tail.y][tail.x] = true;
             }
         }
+        //printVisited(visited);
     }
 
     int visitedCount = 0;
